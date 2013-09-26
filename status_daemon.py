94abfc0e46af4371074b62b40ad1c921728f8646
Andrew Orr <andrew@andreworr.ca>
2013-09-26 00:28:14 +0000

    lol

--- status_daemon.py
+++ status_daemon.py
@@ -16,9 +16,12 @@ LCD.add_button_callback(LCD.BUTTON_3, le_callback, LCD.BUTTON_EITHER)
 LCD.add_button_callback(LCD.BUTTON_4, le_callback, LCD.BUTTON_EITHER)
 LCD.wait_for_buttons()
 
-while True:
-	LCD.set_cursor(0, 0)
-	LCD.print_string(time.strftime('%X'))
-	LCD.set_cursor(0, 1)
-	LCD.print_string(netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr'])
+try:
+	while True:
+		LCD.set_cursor(0, 0)
+		LCD.print_string(time.strftime('%X'))
+		LCD.set_cursor(0, 1)
+		LCD.print_string(netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr'])
 	time.sleep(1)
+except (RuntimeError, TypeError, NameError, ValueError):
+	pass
